bb56207c010355d82aa325999637dd766332dd9c
Jan van heusde <janvheusde@gmail.com>
2024-06-17 14:04:18 +0100

    saving before rebooting

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import {lusitana} from '@/app/ui/fonts'
 export default function Page() {
   return (
     <main className="flex bg-seashell min-h-screen flex-col p-6">
-      <div className="flex h-10 shrink-0 items-center justify-center rounded-lg bg-seashell p-4 md:h-32">
-        <p className={`${lusitana.className} antialiased`}>The people times</p>
+      <div className="sticky top-0 flex h-10 shrink-0 items-center justify-center rounded-lg bg-seashell p-4 md:h-32">
+      <span className={`${lusitana.className} antialiased text-4xl`}>The people times</span>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
